0f85935fd484d3ee18c799fe14ac59710c1cdf03
Fitzgerald Challar <challararise@gmail.com>
2026-08-19 12:23:06 +0000

    fixed whitenoise

--- config/settings.py
+++ config/settings.py
@@ -228,14 +228,21 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": (
+            "whitenoise.storage."
+            "CompressedManifestStaticFilesStorage"
+        ),
+    },
+}
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
-
-# WhiteNoise
-STATICFILES_STORAGE = (
-    "whitenoise.storage.CompressedManifestStaticFilesStorage"
-)
 
 
 # =============================================================================
